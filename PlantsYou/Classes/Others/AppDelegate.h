e23405d716e9b2445ed6fbfbc62f2473ee912803
Initial-C-William Chang <iwilliamchang@outlook.com>
2016-07-26 12:42:41 +0800

    创建工程

---
+++ PlantsYou/Classes/Others/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlantsYou
+//
+//  Created by Mr.C on 16/7/25.
+//  Copyright © 2016年 Mr.C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlantsYou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlantsYou
+//
+//  Created by Mr.C on 16/7/25.
+//  Copyright © 2016年 Mr.C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
